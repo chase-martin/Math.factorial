7569c829a8a3bce33a80bec95e1aaaacbf50fc7b
Chase Martin <chase.martin@gmail.com>
2013-04-30 02:19:52 -0700

    Update Math.factorial.js
    Adding in the better algorithm for comparison

--- Math.factorial.js
+++ Math.factorial.js
@@ -1,6 +1,6 @@
 // A (very slow) factorial algorithm
 // Uses an Adaptive Order Y Combinator pattern (http://en.wikipedia.org/wiki/Y_combinator)
-// Adapted from Douglas Crockfords "The Little JavaScripter": http://javascript.crockford.com/little.html
+// Adapted (just for fun) from Douglas Crockfords "The Little JavaScripter"
 // See FactorialAlgorithmComparisionTests.js in this repo for more efficient algorithms
 // An iterative approach using memoization proved fastest by far in the above tests
 
@@ -15,3 +15,18 @@ Math.factorial = (function callRecursively(factorialFunction) {
 })(function (_factorial) {
 	return function (n) { return n <= 2 ? n : n * _factorial(n - 1); };
 });
+
+// Iterative algorithm using memoization
+// This algorithm proved the fastest in my testing
+// See FactorialAlgorithmComparisionTests.js in this repo
+Math.mFactorial = function(n) {
+	this.facts = this.facts || [];
+	if (n==0 || n==1) {
+		return 1;
+	}
+	if(this.facts[n]>0) {
+		return this.facts[n];
+	} else {
+		return this.facts[n] = memoizationFactorial(n-1)*n;
+	}
+};
